3e6eac61448e26f6e7fe976b2ec461ec2e1f9db3
jmcbride <jmcbride@uccs.edu>
2019-10-12 12:20:20 -0600

    Test Database commit

--- test-api/src/Test.java
+++ test-api/src/Test.java
@@ -1,8 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is a test");
-        System.out.println("This is Test 2");
-        System.out.println("This is the Third Test");
-        System.out.print("This is a 4th test");
+        try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs_3300_project", "postgres", "password")) {
+            System.out.println("Java JDBC Example");
+
+            System.out.println("Connected to PostgreSQL Database!");
+        }
+        catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }
