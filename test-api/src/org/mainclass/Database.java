c95dc53dfe0835e663156201e87d0d5098fa5196
jmcbride <jmcbride@uccs.edu>
2019-10-17 15:13:04 -0600

    New Code to test JDBC, JavaFX, and packages

--- test-api/src/org/mainclass/Database.java
+++ test-api/src/org/mainclass/Database.java
@@ -1,15 +1,16 @@
+package org.mainclass;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Test {
-    public static void main(String[] args) {
-        try(Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs_3300_project", "postgres", "password")) {
+public class Database {
+    public static void testDatabase() {
+        try (Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/cs_3300_project", "postgres", "password")) {
             System.out.println("Java JDBC Example");
 
             System.out.println("Connected to PostgreSQL Database!");
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println("Connection failure.");
             e.printStackTrace();
         }
---
+++ test-api/src/org/mainclass/Test.java
@@ -0,0 +1,31 @@
+package org.mainclass;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Test extends Application {
+    public static void main(String[] args) {
+        Database.testDatabase();
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Title of Window");
+
+        Button button = new Button("Click me");
+
+        StackPane layout = new StackPane();
+
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
