3e58acf478629fb26f86835e6b059bb122321886
Mimi Namsinh <miminamsinh@Mimis-MacBook-Pro.local>
2019-11-19 15:54:46 -0700

    Merge branch 'master' of /Users/miminamsinh/IdeaProjects/cs-3300-project with conflicts.

--- test-api/src/org/Main.java
+++ test-api/src/org/Main.java
@@ -4,19 +4,36 @@ import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.controllers.LoginController;
 
 public class Main extends Application {
-    public static void main(String[] args) { launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
-    public void start(Stage stage) {
-        LoginController login = new LoginController(stage, "../fxmls/login.fxml");
-        Scene scene = new Scene( login);
+//    public void start(Stage stage) {
+//        LoginController login = new LoginController(stage, "../fxmls/login.fxml");
+//        Scene scene = new Scene( login, 800, 400);
+//
+//        stage.setTitle("Clyde's Pickup - Login");
+//
+//        stage.setScene(scene);
+//
+////        stage.show();
+//    }
 
-        stage.setTitle("Clyde's Pickup - Login");
+    public void start(Stage stage) {
+
+        //LoginController login = new LoginController(stage, "../fxmls/mainMenu.fxml");
+        LoginController mainMenu = new LoginController(stage, "../fxmls/mainMenu.fxml");
+
+        Scene scene = new Scene( mainMenu);
+
+        stage.setTitle("Clyde's Pickup - Main Menu");
 
         stage.setScene(scene);
 
