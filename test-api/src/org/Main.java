651ce0dac29b342d12de49f64c666f53d55b8cab
miminamsinh <m_namsinh@yahoo.com>
2019-11-19 17:47:01 -0700

    created mainMenu scene

--- test-api/src/org/Main.java
+++ test-api/src/org/Main.java
@@ -15,28 +15,15 @@ public class Main extends Application {
     }
 
     @Override
-//    public void start(Stage stage) {
-//        LoginController login = new LoginController(stage, "../fxmls/login.fxml");
-//        Scene scene = new Scene( login, 800, 400);
-//
-//        stage.setTitle("Clyde's Pickup - Login");
-//
-//        stage.setScene(scene);
-//
-////        stage.show();
-//    }
-
     public void start(Stage stage) {
+        LoginController login = new LoginController(stage, "../fxmls/login.fxml");
+        Scene scene = new Scene( login, 800, 400);
 
-        //LoginController login = new LoginController(stage, "../fxmls/mainMenu.fxml");
-        LoginController mainMenu = new LoginController(stage, "../fxmls/mainMenu.fxml");
-
-        Scene scene = new Scene( mainMenu);
-
-        stage.setTitle("Clyde's Pickup - Main Menu");
+        stage.setTitle("Clyde's Pickup - Login");
 
         stage.setScene(scene);
 
         stage.show();
     }
+
 }
